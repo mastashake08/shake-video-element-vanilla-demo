066c5d7baeb918b298214b5434420b3e16e54eb7
Hacker God <jyrone.parker@gmail.com>
2023-12-03 17:23:12 -0500

    testing

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,3 @@ import '../css/global.css';
 import '../scss/global.scss';
 import { registerElement } from '@mastashake08/shake-video-element';
 registerElement();
-const shake = document.getElementById('shake-video');
-console.log(shake.getVideo())
